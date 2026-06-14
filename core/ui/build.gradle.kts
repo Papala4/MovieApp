4977456697f837bf711b4027a1f6334e0f3929ff
n.papalashvili <n.papalashvili@space.ge>
2026-06-15 02:48:55 +0400

    add dependency

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.core.ktx)
     implementation(libs.material)
+    implementation(libs.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
