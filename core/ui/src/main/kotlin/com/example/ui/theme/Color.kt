a0033336a9c85ea2134968e38b3942b6926b2318
n.papalashvili <n.papalashvili@space.ge>
2026-06-18 13:54:31 +0400

    fix review comments

--- core/ui/src/main/kotlin/com/example/ui/theme/Color.kt
+++ core/ui/src/main/kotlin/com/example/ui/theme/Color.kt
@@ -21,7 +21,7 @@ data class MovieAppColors(
 )
 
 private val YellowPrimary = Color(0xFFFFC44A)
-private val Neutral01Black = Color(0xFF080808)
+private val Neutral01 = Color(0xFF080808)
 private val Neutral02 = Color(0xFF1C1C1C)
 private val Neutral03 = Color(0xFF5D5D5D)
 private val Neutral04 = Color(0xFF808080)
@@ -29,22 +29,26 @@ private val Neutral05 = Color(0xFFA5A5A5)
 private val Neutral06 = Color(0xFFCACACA)
 private val Neutral07 = Color(0xFFDEDEDE)
 private val Neutral08 = Color(0xFFEAEAEA)
+private val WarningColor = Color(0xFFFFEB3B)
+private val SuccessColor = Color(0xFF4CAF50)
+private val ErrorColor = Color(0xFFFC0000)
+
 
 val DarkMovieColors = MovieAppColors(
     primary = YellowPrimary,
-    background = Neutral01Black,
+    background = Neutral01,
     surface = Neutral02,
     surfaceVariant = Neutral03,
-    onPrimary = Neutral01Black,
+    onPrimary = Neutral01,
     onBackground = Neutral08,
-    onSurface = Neutral08,
+    onSurface = Neutral07,
     textPrimary = Neutral08,
-    textSecondary = Neutral05,
-    border = Neutral03,
-    error = Color(0xFFCF6679),
-    success = Color(0xFF4CAF50),
-    warning = YellowPrimary,
-    overlay = Color(0x99000000)
+    textSecondary = Neutral06,
+    border = Neutral04,
+    overlay = Neutral05,
+    error = ErrorColor,
+    success = SuccessColor,
+    warning = WarningColor,
 )
 
 val LocalMovieColors = staticCompositionLocalOf { DarkMovieColors }
