7a44f8861565538439e920c4cbe2d66668405346
n.papalashvili <n.papalashvili@space.ge>
2026-07-16 18:03:03 +0400

    add favorite handling in Home vm.

--- feature/home/presentation/src/main/kotlin/com/space/home/di/HomeVmModule.kt
+++ feature/home/presentation/src/main/kotlin/com/space/home/di/HomeVmModule.kt
@@ -6,6 +6,6 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val homeVmModule = module {
-    factory { MovieUiMapper() }
-    viewModel { HomeViewModel(get(), get(), get(), get(), get()) }
+    single { MovieUiMapper() }
+    viewModel { HomeViewModel(get(), get(), get(), get(), get(), get(), get(), get()) }
 }
