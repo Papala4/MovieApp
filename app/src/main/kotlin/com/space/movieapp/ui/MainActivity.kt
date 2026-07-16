fd420c1fffe6a075de13121b21dcf3bc15525bf8
n.papalashvili <n.papalashvili@space.ge>
2026-07-16 15:57:53 +0400

    add No Internet Banner for paging.

--- app/src/main/kotlin/com/space/movieapp/ui/MainActivity.kt
+++ app/src/main/kotlin/com/space/movieapp/ui/MainActivity.kt
@@ -8,18 +8,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.space.home.screen.HomeScreen
-import com.space.ui.component.banners.NoInternetBanner
 import com.space.ui.theme.MovieAppTheme
 import com.space.ui.theme.MovieTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -41,29 +35,16 @@ class MainActivity : ComponentActivity() {
         )
         setContent {
             MovieAppTheme {
-                val isOnline by vm.isOnline.collectAsStateWithLifecycle()
-
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
                         .background(MovieTheme.colors.background)
                 ) {
                     Scaffold { innerPadding ->
-//                        MovieDetailsScreen(
-//                            movieId = 4347,
-//                            modifier = Modifier.padding(paddingValues = innerPadding)
-//                        )
-
                         HomeScreen(
                             modifier = Modifier.padding(paddingValues = innerPadding)
-                        ) // just for test Todo{delete before merge}
+                        )
                     }
-
-                    NoInternetBanner(
-                        visible = !isOnline,
-                        windowInsets = WindowInsets.navigationBars,
-                        modifier = Modifier.align(Alignment.BottomCenter)
-                    )
                 }
             }
         }
